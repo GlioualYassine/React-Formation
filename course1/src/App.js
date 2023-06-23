b5002098583847c4a5410c1b4429cf8d0d4aa047
GlioualYassine <glioualyassine@gmail.com>
2023-06-23 09:32:07 +0100

    finish cours 1

--- course1/src/App.js
+++ course1/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-
+import Product from './components/product';
+import Nav from './components/nav';
 function App() {
   return (
     <div className="App">
         <>
+        <Nav/>
           <div>
-            
+            <Product/>
           </div>
         </>
     </div>
